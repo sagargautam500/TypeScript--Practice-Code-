addd5d3635eb61aa70e2646fc2c8b4b53c9df308
Sagar Gautam <sagargautam389@gmail.com>
2025-02-16 15:17:58 +0545

    doneproject

--- 1_Project-HtmlCssTs-withParcel/dist/index.377278e2.js
+++ 1_Project-HtmlCssTs-withParcel/dist/index.377278e2.js
@@ -1,3 +1,22 @@
 console.log("this is first website using typescript:");
+const btn = document.querySelector(".clickMe");
+// const body=document.body as HTMLElement;
+const bodyEle = document.body;
+let isWhite = true;
+let color = [
+    "red",
+    "yellow",
+    "orange",
+    "blue",
+    "pink",
+    "brown"
+];
+// let ranNum:number=Math.floor(Math.random() * 7);
+btn.addEventListener("click", ()=>{
+    // console.log(" i am click")
+    if (isWhite) bodyEle.style.backgroundColor = color[Math.floor(Math.random() * 7)];
+    else bodyEle.style.backgroundColor = "";
+    isWhite = !isWhite;
+});
 
 //# sourceMappingURL=index.377278e2.js.map
--- 1_Project-HtmlCssTs-withParcel/src/index.ts
+++ 1_Project-HtmlCssTs-withParcel/src/index.ts
@@ -1,1 +1,17 @@
 console.log("this is first website using typescript:");
+const btn=document.querySelector(".clickMe") as HTMLButtonElement;
+// const body=document.body as HTMLElement;
+const bodyEle:HTMLElement=document.body ;
+let isWhite:boolean=true;
+let color:string[]=["red","yellow","orange","blue","pink","brown"]
+// let ranNum:number=Math.floor(Math.random() * 7);
+
+btn.addEventListener("click",()=>{
+  // console.log(" i am click")
+  if(isWhite){
+    bodyEle.style.backgroundColor=color[Math.floor(Math.random() * 7)];
+  }else{
+    bodyEle.style.backgroundColor="";
+  }
+  isWhite=!isWhite;
+})
