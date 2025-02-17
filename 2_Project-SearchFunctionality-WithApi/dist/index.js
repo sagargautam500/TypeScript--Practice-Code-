13c3b3af148a376a9625d6fed966a02fc99bcdac
Sagar Gautam <sagargautam389@gmail.com>
2025-02-17 15:00:34 +0545

    searchFunctionalityProject-Done

--- 2_Project-SearchFunctionality-WithApi/dist/index.js
+++ 2_Project-SearchFunctionality-WithApi/dist/index.js
@@ -1,2 +1,65 @@
 "use strict";
-console.log("hello sagar");
+// const formSubmit:HTMLFormElement |null =document.querySelector("#form");
+const formSubmit = document.querySelector("#form");
+const getUserName = document.querySelector("#user");
+const mainContainer = document.querySelector(".main_container");
+// const responseData = async (): Promise<any[]> => {
+//   const data: Response = await fetch("https://api.github.com/users");
+//   const response: any[] = await data.json();
+//   return  response;
+// };
+// // Fetching data
+// responseData()
+// .then((fetchData)=>console.log(fetchData))
+const fetchingData = async () => {
+    try {
+        const response = await fetch("https://api.github.com/users");
+        if (!response.ok) {
+            throw new Error(`HTTP Error! Status: ${response.status}`);
+        }
+        return await response.json(); // Returning JSON data directly
+    }
+    catch (error) {
+        console.error("Error fetching data:", error);
+        return null; // Return null if there's an error
+    }
+};
+// Fetch data
+fetchingData()
+    .then((data) => {
+    data.map((singleUser) => {
+        showResultUi(singleUser);
+    });
+});
+function showResultUi(singleUser) {
+    const { avatar_url, login, url } = singleUser;
+    mainContainer.insertAdjacentHTML("beforeend", `
+     <div class="card">
+        <div class="card-img">
+           <img src=${avatar_url} alt=${login}></img>
+        <div>
+        <div class="card-footer">
+           <img src=${avatar_url} alt=${login} />
+           <p>${login}</p>
+           <a href=${url}>Github</a>
+        </div>
+      </div>
+    `);
+}
+//let's search functionality
+formSubmit.addEventListener("input", async (e) => {
+    e.preventDefault();
+    const searchName = getUserName.value.toLowerCase();
+    // Fetch data
+    const allUserData = await fetchingData();
+    const matchingUser = allUserData.filter((user) => {
+        return user.login.toLowerCase().includes(searchName);
+    });
+    mainContainer.innerHTML = "";
+    if (matchingUser.length === 0) {
+        mainContainer.insertAdjacentHTML("beforeend", `<p>No matching user data?</p>`);
+    }
+    else {
+        matchingUser.map((signalUser) => showResultUi(signalUser));
+    }
+});
--- 2_Project-SearchFunctionality-WithApi/src/index.ts
+++ 2_Project-SearchFunctionality-WithApi/src/index.ts
@@ -1,1 +1,74 @@
-console.log("hello sagar");
+// const formSubmit:HTMLFormElement |null =document.querySelector("#form");
+const formSubmit = document.querySelector("#form") as HTMLFormElement;
+const getUserName = document.querySelector("#user") as HTMLInputElement;
+const mainContainer = document.querySelector(".main_container") as HTMLElement;
+
+// const responseData = async (): Promise<any[]> => {
+//   const data: Response = await fetch("https://api.github.com/users");
+//   const response: any[] = await data.json();
+//   return  response;
+// };
+
+// // Fetching data
+// responseData()
+// .then((fetchData)=>console.log(fetchData))
+
+const fetchingData = async (): Promise<any> => {
+  try {
+    const response = await fetch("https://api.github.com/users");
+
+    if (!response.ok) {
+      throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+
+    return await response.json(); // Returning JSON data directly
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null; // Return null if there's an error
+  }
+};
+// Fetch data
+fetchingData()
+  .then((data) => {
+    data.map((singleUser: any) => {
+      showResultUi(singleUser)
+    });
+  });
+
+function showResultUi(singleUser: any) {
+  const { avatar_url, login, url } = singleUser;
+  mainContainer.insertAdjacentHTML("beforeend",
+    `
+     <div class="card">
+        <div class="card-img">
+           <img src=${avatar_url} alt=${login}></img>
+        <div>
+        <div class="card-footer">
+           <img src=${avatar_url} alt=${login} />
+           <p>${login}</p>
+           <a href=${url}>Github</a>
+        </div>
+      </div>
+    `)
+}
+
+
+//let's search functionality
+formSubmit.addEventListener("input",async(e)=>{  //"submit" used then click or press then work done.
+  e.preventDefault();
+  const searchName=getUserName.value.toLowerCase();
+
+  // Fetch data
+const allUserData=await fetchingData();
+const matchingUser=allUserData.filter((user: { login: string; })=>{
+return user.login.toLowerCase().includes(searchName)
+})
+mainContainer.innerHTML=""
+if(matchingUser.length===0){
+  mainContainer.insertAdjacentHTML("beforeend",
+    `<p>No matching user data?</p>`
+  )
+  }else{
+    matchingUser.map((signalUser: any)=>showResultUi(signalUser))
+  }
+});
